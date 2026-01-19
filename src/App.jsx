edd8f9d6452076225f943b5d86a7ee475bcd19ce
Adim806 <adim806@gmail.com>
2026-01-19 13:56:34 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Hero } from '@/sections/Hero'
+import { About } from '@/sections/About'
+import { Navbar } from '@/layout/Navbar'
+import { Experience } from '@/sections/Experience'
+import { Projects } from '@/sections/Projects'
+import { Testimonials } from '@/sections/Testimonials'
+import { Contact } from '@/sections/Contact'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+       <Navbar /> 
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Testimonials />
+        <Contact />
+  
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
